92020468aa522e0c9633b9f073b55bf5bba0e11f
= <=>
2024-08-06 22:19:46 -0700

    fix: select search

--- src/components/ui/form/selector/content.tsx
+++ src/components/ui/form/selector/content.tsx
@@ -5,50 +5,50 @@ import { Check } from "lucide-react";
 import { cn } from "~/lib/utils";
 
 type SelectorContentProps<Option> = {
-  onSelect(option?: Option): void;
-  getValue<T>(option: Option): T;
-  getLabel<T>(option: Option): T;
-  getIsSelect(option: Option): boolean;
-  options: Option[];
+	onSelect(option?: Option): void;
+	getValue<T>(option: Option): T;
+	getLabel<T>(option: Option): T;
+	getIsSelect(option: Option): boolean;
+	options: Option[];
 } & Pick<SelectorCommonProps, "searchable">;
 
 export function SelectorContent<Option extends TOption>(
-  props: SelectorContentProps<Option>
+	props: SelectorContentProps<Option>,
 ) {
-  return (
-    <Popover.Content className="w-[200px] p-0" align="start">
-      <Command.Root>
-        {props.searchable && (
-          <Command.Input className="text-xs" placeholder="Search" />
-        )}
+	return (
+		<Popover.Content className="w-[200px] p-0" align="start">
+			<Command.Root>
+				{props.searchable && (
+					<Command.Input className="text-xs" placeholder="Search" />
+				)}
 
-        <Command.List>
-          <Command.Empty>No language found.</Command.Empty>
-          <Command.Group>
-            {props.options.map((option) => {
-              const optionValue = props.getValue(option);
+				<Command.List>
+					<Command.Empty>No language found.</Command.Empty>
+					<Command.Group>
+						{props.options.map((option) => {
+							const optionValue = props.getValue(option);
 
-              return (
-                <Command.Item
-                  value={String(optionValue)}
-                  key={String(optionValue)}
-                  className="text-xs"
-                  onSelect={() => props.onSelect(option)}
-                >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      props.getIsSelect(option) ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {props.getLabel(option)}
-                </Command.Item>
-              );
-            })}
-          </Command.Group>
-        </Command.List>
-      </Command.Root>
-      <Popover.Arrow className="fill-gray-6 dark:fill-graydark-6" />
-    </Popover.Content>
-  );
+							return (
+								<Command.Item
+									value={optionValue as string}
+									key={optionValue as string}
+									className="text-xs"
+									onSelect={() => props.onSelect(option)}
+								>
+									<Check
+										className={cn(
+											"mr-2 h-4 w-4",
+											props.getIsSelect(option) ? "opacity-100" : "opacity-0",
+										)}
+									/>
+									{props.getLabel(option)}
+								</Command.Item>
+							);
+						})}
+					</Command.Group>
+				</Command.List>
+			</Command.Root>
+			<Popover.Arrow className="fill-gray-6 dark:fill-graydark-6" />
+		</Popover.Content>
+	);
 }
