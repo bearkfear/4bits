af9fe6ca2e8ed3e5dfe9692fa8c7059e65241f1e
Enrico <camponogara5@gmail.com>
2024-08-09 14:15:18 -0300

    fix: import

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -4,7 +4,7 @@ import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
 import * as React from "react";
 
-import { buttonVariants } from "dist";
+import { buttonVariants } from "./button";
 import { cn } from "~/lib/utils";
 import { Separator } from "./separator";
 
