a93ec02a9ee738723ecf3acc5f6290d215be8aba
Enrico <camponogara5@gmail.com>
2024-08-23 14:06:40 -0300

    fix(version): new version and build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,17 +3,12 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   // The file we created above that will be the entrypoint to the library.
   entry: ["src/index.ts"],
-  // Enable TypeScript type definitions to be generated in the output.
-  // This provides type-definitions to consumers.
-  dts: true,
-  // Clean the `dist` directory before building.
-  // This is useful to ensure the output is only the latest.
-  clean: true,
-  // Sourcemaps for easier debugging.
+  minify: true,
+  target: "es2018",
+  external: ["react"],
   sourcemap: true,
-  splitting: true,
-  target: "es2019",
-  format: ["cjs", "esm"],
+  dts: true,
+  format: ["esm", "cjs"],
   esbuildOptions(options) {
     options.banner = {
       js: '"use client"',
