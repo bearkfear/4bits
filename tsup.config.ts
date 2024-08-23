768f50857a7ce2fdc0e1cf7e750ba6587f934a59
Enrico <camponogara5@gmail.com>
2024-08-23 14:28:36 -0300

    fix(build): corrige build

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,16 +2,12 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   // The file we created above that will be the entrypoint to the library.
-  entry: ["src/index.ts"],
+  entry: ["src/**/*.ts", "src/**/*.tsx", "!src/stories", "src/styles/*.css"],
   minify: true,
   target: "es2018",
   external: ["react"],
   sourcemap: true,
   dts: true,
   format: ["esm", "cjs"],
-  esbuildOptions(options) {
-    options.banner = {
-      js: '"use client"',
-    };
-  },
+  bundle: false,
 });
