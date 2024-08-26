b6cb80b92f45a723e201ef158cd7fef83fced8b5
Enrico <camponogara5@gmail.com>
2024-08-26 09:04:39 -0300

    fix(ui): corrige tsconfig

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   // The file we created above that will be the entrypoint to the library.
   entry: ["src/**/*.ts", "src/**/*.tsx", "!src/stories", "src/styles/*.css"],
-  minify: true,
+  minify: false,
   target: "es2018",
-  external: ["react"],
+  external: ["react", "react-dom"],
   sourcemap: true,
   dts: true,
   format: ["esm", "cjs"],
   bundle: false,
-  tsconfig: "./tsconfig.json"
+  tsconfig: "./tsconfig.json",
+  clean: true
 });
