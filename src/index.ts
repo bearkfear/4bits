0e3999874bd75a61ca6a2e79ba86b96122d1d9ec
Enrico <camponogara5@gmail.com>
2024-08-26 10:09:29 -0300

    fix(packages): corrige versao dos pacotes e exports nomeados

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,34 @@
 import "./styles/globals.css";
 
 // Others exports for who is going to use the library
-export * from "./lib/utils";
+export { cn } from "./lib/utils";
 export * as AlertDialog from "./components/ui/alert-dialog";
-export * from "./components/ui/button";
+export { Button, buttonVariants } from "./components/ui/button";
+export type { ButtonProps } from "./components/ui/button";
 export * as Command from "./components/ui/command";
-export * from "./components/ui/loader";
-export * from "./components/ui/page-loader";
+export { Loader } from "./components/ui/loader";
+export type { LoaderProps } from "./components/ui/loader";
+export { PageLoader } from "./components/ui/page-loader";
 export * as Popover from "./components/ui/popover";
-export * from "./components/ui/toaster";
+export { Toaster, toast } from "./components/ui/toaster";
 export * as Tooltip from "./components/ui/tooltip";
-export * from "./components/ui/separator";
+export { Separator } from "./components/ui/separator";
 export * as Dialog from "./components/ui/dialog";
 export * as Dropdown from "./components/ui/dropdown";
 
 // export forms
 
-export * from "./components/ui/form/checkbox";
-export * from "./components/ui/form/builder";
-export * from "./components/ui/form/helper-text";
-export * from "./components/ui/form/input";
-export * from "./components/ui/form/label";
-export * from "./components/ui/form/textarea";
-export * from "./components/ui/form/selector";
+export { Checkbox } from "./components/ui/form/checkbox";
+export { FormBuilder } from "./components/ui/form/builder";
+export { HelperText } from "./components/ui/form/helper-text";
+export type { HelperTextProps } from "./components/ui/form/helper-text";
+export { Input, inputVariants } from "./components/ui/form/input";
+export type { InputProps } from "./components/ui/form/input";
+export { Label } from "./components/ui/form/label";
+export { Textarea } from "./components/ui/form/textarea";
+export type { TextareaProps } from "./components/ui/form/textarea";
+export { MultiSelector, SingleSelector } from "./components/ui/form/selector";
+export type {
+  MultiSelectorProps,
+  SingleSelectorProps,
+} from "./components/ui/form/selector";
