2ae3e806d1568a3994cfb1351cce1a9642808324
Enrico <camponogara5@gmail.com>
2024-09-02 15:20:20 -0300

    fix(packages): ts up and packages

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  // The file we created above that will be the entrypoint to the library.
-  entry: ["src/**/*.ts", "src/**/*.tsx", "!src/stories", "src/styles/*.css"],
-  minify: true,
-  target: "es2018",
-  external: ["react"],
-  sourcemap: true,
-  dts: true,
-  format: ["esm", "cjs"],
-  bundle: false,
-  tsconfig: "./tsconfig.json"
+	// The file we created above that will be the entrypoint to the library.
+	entry: ["src/**/*.ts", "src/**/*.tsx", "!src/stories", "src/styles/*.css"],
+	minify: true,
+	target: "es2018",
+	external: ["react", "react-dom"],
+	sourcemap: false,
+	dts: true,
+	format: ["esm", "cjs"],
+	clean: true,
+	bundle: false,
+	tsconfig: "./tsconfig.json",
 });
