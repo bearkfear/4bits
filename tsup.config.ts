082adca42761715b2d624dbe59b1002a9bdd707b
Enrico <camponogara5@gmail.com>
2024-09-27 13:54:15 -0300

    fix(version): corrige version and build

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,12 +8,6 @@ export default defineConfig({
 	external: ["react", "react-dom"],
 	sourcemap: false,
 	dts: true,
-	format: ["cjs", "esm"],
-	outExtension(ctx) {
-		return {
-			js: ctx.format === "esm" ? ".mjs" : ".js",
-		};
-	},
 	clean: true,
 	bundle: false,
 	tsconfig: "./tsconfig.json",
