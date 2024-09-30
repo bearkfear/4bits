78136bb7e744e199f7c6b25a817ff61e93369264
Enrico <camponogara5@gmail.com>
2024-09-30 15:08:23 -0300

    fix(build): corrige build packages

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from "tsup";
+import { defineConfig, Format } from "tsup";
 
 export default defineConfig({
 	// The file we created above that will be the entrypoint to the library.
-	entry: ["src", "!src/stories"],
+	entry: ["src/**/*.{ts,tsx}", "src/styles/globals.css", "!src/stories"],
 	target: "es2019",
 	external: ["react", "react-dom"],
 	dts: true,
 	clean: true,
 	format: ["cjs", "esm"],
 	tsconfig: "./tsconfig.json",
-	bundle: true,
+	platform: "browser",
+	bundle: false,
+	legacyOutput: true,
+
+	// splitting: true,
+	// treeshake: true,
 });
