818bb96fc7b2ca53bf9421be136ad3669e273d0a
YuriAtzler <atzleryuri@gmail.com>
2024-12-05 15:11:46 -0300

    feat: exporta button do action tablebuilder

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -17,7 +17,10 @@ export { Stepper } from "./components/ui/stepper";
 export { Tabs } from "./components/ui/tabs";
 export { Card } from "./components/ui/card";
 export { Table } from "./components/ui/table";
-export { TableBuilder } from "./components/ui/table-builder/table";
+export {
+	TableBuilder,
+	TableBuilderActionButton,
+} from "./components/ui/table-builder";
 export type { TableBuilderProps } from "./components/ui/table-builder/types";
 export { Pagination } from "./components/ui/pagination";
 export {
