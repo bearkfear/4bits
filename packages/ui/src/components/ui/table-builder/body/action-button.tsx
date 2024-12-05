481888c74e9a0a28940e32dd66b77ac4ace0a112
YuriAtzler <atzleryuri@gmail.com>
2024-12-05 15:21:10 -0300

    fix: ajusta botao de acao de tablebuilder

--- packages/ui/src/components/ui/table-builder/body/action-button.tsx
+++ packages/ui/src/components/ui/table-builder/body/action-button.tsx
@@ -1,19 +1,18 @@
 import { type VariantProps, cva } from "class-variance-authority";
 import type { ReactNode } from "react";
-import { Dropdown } from "../../dropdown";
 
 const actionVariants = cva(
-  "space-x-1 cursor-pointer hover:opacity-95 px-3 rounded text-gray-12 dark:text-graydark-12",
+  "space-x-1 cursor-pointer w-full flex items-center text-sm py-1 px-3 rounded text-gray-12 dark:text-graydark-12",
   {
     variants: {
       variant: {
-        default: "",
         hidden: "hidden",
-        info: "bg-blue-9 dark:bg-bluedark-8 hover:!bg-blue-10 dark:hover:!bg-bluedark-9 text-graydark-12",
+        default: "hover:bg-gray-3 dark:hover:bg-graydark-3",
+        info: "bg-blue-9 dark:bg-bluedark-9 hover:!bg-blue-10 dark:hover:!bg-bluedark-9 text-graydark-12",
         success:
-          "bg-green-9 dark:bg-greendark-8 hover:!bg-green-10 dark:hover:!bg-greendark-9 text-graydark-12",
+          "bg-green-9 dark:bg-greendark-9 hover:!bg-green-10 dark:hover:!bg-greendark-9 text-graydark-12",
         danger:
-          "bg-red-9 dark:bg-reddark-8 hover:!bg-red-10 dark:hover:!bg-reddark-9 text-graydark-12",
+          "bg-red-9 dark:bg-reddark-9 hover:!bg-red-10 dark:hover:!bg-reddark-9 text-graydark-12",
       },
     },
   }
@@ -37,9 +36,13 @@ export function TableBuilderActionButton({
   label,
 }: TableBuilderActionButtonProps) {
   return (
-    <Dropdown.Item className={actionVariants({ variant })} onClick={onClick}>
+    <button
+      type="button"
+      className={actionVariants({ variant })}
+      onClick={onClick}
+    >
       <div className="w-4 flex items-center justify-center">{icon}</div>
       <span>{label}</span>
-    </Dropdown.Item>
+    </button>
   );
 }
